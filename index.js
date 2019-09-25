c430da8330529c9f199c7c95c5ff7b366fe16afd
Michael O'Brien <mob@emobrien.com>
2019-09-26 08:37:58 +1000

    DEV: bump version

--- index.js
+++ index.js
@@ -192,13 +192,3 @@ Object.black = function(obj, mask) {
     }
     return result
 }
-
-/*
-    randomize(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]]
-        }
-        return array
-    }
-*/
